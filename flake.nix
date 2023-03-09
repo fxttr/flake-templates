a21c097c871d6c75613115938bdc0c7975872c22
Florian Büstgens <fb@fx-ttr.de>
2023-03-09 20:25:55 +0100

    Updating agda template

--- flake.nix
+++ flake.nix
@@ -16,7 +16,12 @@
 
       ocaml = {
         path = ./ocaml;
-        description = "OCaml template using the dune buildsystem"
+        description = "OCaml template using the dune buildsystem";
+      };
+
+      agda = {
+        path = ./agda;
+        description = "Agda template using the default agda package";
       };
 
       trivial = {

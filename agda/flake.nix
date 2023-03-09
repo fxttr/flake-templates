dbb6470e55a7c7945b5788cdb688c066c4aff8a7
Florian Büstgens <fb@fx-ttr.de>
2023-03-09 20:24:19 +0100

    Add agda flake

---
+++ agda/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Agda Template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+      flake-utils.lib.eachDefaultSystem
+        (system:
+          let pkgs = nixpkgs.legacyPackages.${system}; in
+          {
+            inherit pkgs;
+            devShells.default = pkgs.mkShell {
+                buildInputs = [
+                  pkgs.agda.withPackages (p: [ p.standard-library ])
+                ];
+              };
+          }
+        );
+}
